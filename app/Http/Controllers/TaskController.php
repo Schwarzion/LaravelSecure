2a9c4ac1b57e2ff56129fef9b6e54028542e22e4
Schwarzion <victor.lehouck@gmail.com>
2020-01-21 10:17:29 +0100

    [init]

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use JWTAuth;
+use App\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * @var
+     */
+    protected $user;
+
+    /**
+     * @return mixed
+     */
+    public function index()
+    {
+        $this->user = JWTAuth::parseToken()->authenticate();
+        if ($this->user) {
+            return response()->json([
+                'success' => true,
+                'tasks' => $this->user->tasks()->get(['title', 'description'])->toArray()
+            ], 400);
+        }
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $task = $this->user->tasks()->find($id);
+
+        if (!$task) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task with id ' . $id . ' cannot be found.'
+            ], 400);
+        }
+
+        return $task;
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->description = $request->description;
+
+        if ($this->user->tasks()->save($task))
+            return response()->json([
+                'success' => true,
+                'task' => $task
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task could not be added.'
+            ], 500);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $task = $this->user->tasks()->find($id);
+
+        if (!$task) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task with id ' . $id . ' cannot be found.'
+            ], 400);
+        }
+
+        $updated = $task->fill($request->all())->save();
+
+        if ($updated) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task could not be updated.'
+            ], 500);
+        }
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $task = $this->user->tasks()->find($id);
+
+        if (!$task) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, task with id ' . $id . ' cannot be found.'
+            ], 400);
+        }
+
+        if ($task->delete()) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Task could not be deleted.'
+            ], 500);
+        }
+    }
+}
